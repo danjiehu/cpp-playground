db8d6e1b52cb3666bdf7c9b9ed38485f9a1b8102
Danjie Hu <danjiehu@outlook.com>
2021-10-18 20:53:51 +0800

    example

--- cpp-syntax/clone.cpp
+++ cpp-syntax/clone.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class box
@@ -16,10 +17,29 @@ public:
         *height = heig;
     }
 
-    void show_data(obj)
+    void show_data()
     {
-        cout << "length is" << length << ", its address is " << &length
-             << "\n breadth is" << breadth << ", its address is " << &breadth
-             << "\n height is" << height << ", its address is " << &height << endl;
+        cout << "length is " << length << ", address is " << &length
+             << "\n breadth is " << breadth << ", address is " << &breadth
+             << "\n height is " << *height << ", address is " << height << endl;
     }
+};
+
+int main()
+{
+    box b1;
+
+    b1.set_dimensions(1, 2, 3);
+    b1.show_data();
+
+    //default copy constructor;
+    box b2 = b1;
+    b2.show_data();
+
+    //assigment operator;
+    box b3;
+    b3 = b1;
+    b3.show_data();
+
+    return 0;
 }
