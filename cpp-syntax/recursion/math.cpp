2093381ab6403fa4004c7fbd6f28a282d43c61bd
Danjie Hu <danjiehu@outlook.com>
2021-10-22 16:49:31 +0800

    completed multiplication and factorial recursion solution

--- cpp-syntax/recursion/math.cpp
+++ cpp-syntax/recursion/math.cpp
@@ -7,24 +7,39 @@ int mul(int n, int m)
 {
     if (n == 1)
     {
-        cout << m << endl;
+        int res = m;
+        cout << n << "*" << m << " is: " << res << endl;
         return m;
     }
     else
     {
         int res = mul(n - 1, m) + m;
-        cout << res << endl;
+        cout << n << "*" << m << " is: " << res << endl;
         return res;
     }
 };
 
+int fac(int n)
+{
+    if (n == 1)
+    {
+        int res = 1;
+        cout << "factorial " << n << " is: " << res << endl;
+        return res;
+    }
+    else
+    {
+        int res = n * fac(n - 1);
+        cout << "factorial " << n << " is: " << res << endl;
+        return res;
+    }
+}
+
 int main()
 {
-    // > multiplication
+    // multiplication
     mul(1, 2);
-    // < multiplication
 
-    // > n factorial
-
-    // < n factorial
+    // n factorial
+    fac(5);
 }
