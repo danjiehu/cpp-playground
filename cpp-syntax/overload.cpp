29f84a62f1f30dc75f88811c001a342c8e482331
Danjie Hu <danjiehu@outlook.com>
2021-10-26 23:47:24 +0800

    created member initializer list constructer demo

---
+++ cpp-syntax/overload.cpp
@@ -0,0 +1,19 @@
+/*
+this example contains
+1. operator overload: +, ostream
+2. struct vs. class type, ref http://c.biancheng.net/view/2235.html
+struct vector
+
+*/
+
+#include <iostream>
+#include <string>
+
+struct vector
+{
+    float x, y;
+};
+
+int main()
+{
+}
