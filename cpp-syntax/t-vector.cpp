71751106b46976f311735bdf437fe3623042970c
Danjie Hu <danjiehu@outlook.com>
2021-11-01 21:53:43 +0800

    created t-vector.cpp demo

---
+++ cpp-syntax/t-vector.cpp
@@ -0,0 +1,17 @@
+/*
+    this demo contains: 
+    1. 
+*/
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    std::cout << "Hello world!";
+
+    return 0;
+}
