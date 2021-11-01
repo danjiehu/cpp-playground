5f249e5c0a71634dfbc835b3c47da2d198279c50
Danjie Hu <danjiehu@outlook.com>
2021-11-01 22:23:28 +0800

    vector of user defined types demo completed

--- cpp-syntax/t-vector.cpp
+++ cpp-syntax/t-vector.cpp
@@ -8,10 +8,42 @@
 using std::cout;
 using std::endl;
 
+class Cube {
+private:
+    int length_;
+
+public:
+    Cube(int leng)
+        : length_(leng)
+    {
+    }
+
+    // this function can be public or private, doesn't matter
+    // it allows cout to reference private member length_ outside of the class
+    friend std::ostream& operator<<(std::ostream& stream, const Cube& other)
+    {
+        stream << other.length_ << endl;
+        return stream;
+    }
+};
+
 int main()
 {
+    // below - vector of cubes
+    std::vector<Cube> stack;
+    Cube c1(4);
+    stack.push_back(c1);
+    cout << stack[0] << endl;
 
-    std::cout << "Hello world!";
+    // below - simplest vector int example
+    std::vector<int> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+
+    cout << v[0] << endl;
+    cout << v[1] << endl;
+    cout << v[2] << endl;
 
     return 0;
 }
