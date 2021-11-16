e34e23e0814b5b5f6ab3a8bb197efc2b7c53a1df
Danjie Hu <danjiehu@outlook.com>
2021-11-17 02:27:51 +0800

    update inheritance demo

--- cpp-syntax/inheritance.cpp
+++ cpp-syntax/inheritance.cpp
@@ -9,10 +9,15 @@ public:
         X += xa;
         Y += ya;
     }
+
+    Entity()
+        : X(0)
+        , Y(0) {};
 };
 
-// class Player is now both Player and Entity type (polymorphism)
+// class Player is now both Player and Entity type (TODO: polymorphism)
 // ! Player can access all public members of Entity, but NOT the private members
+// inherited type got all public members of its base type PLUS more
 class Player : public Entity {
 public:
     const char* Name;
@@ -23,10 +28,21 @@ public:
     }
 };
 
+void PrintLocation(Entity e)
+{
+    std::cout << e.X << ", " << e.Y << std::endl;
+}
+
 int main()
 {
     Player player;
+
+    // ex1. Player type accessing its base type public variable and functions
+    std::cout << player.X << std::endl;
     player.Move(1, 1);
+
+    // ex2. passing Player type, even though PrintLocation() accepts Entity type
+    PrintLocation(player);
 
     std::cin.get();
 }
