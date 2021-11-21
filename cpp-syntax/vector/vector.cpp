c814281c950dd58bd08323b2458356d58257bd03
Danjie Hu <danjiehu@outlook.com>
2021-11-21 19:55:51 +0800

    added overloading == example and example code for finding matching instance in a vector

--- cpp-syntax/vector/vector.cpp
+++ cpp-syntax/vector/vector.cpp
@@ -4,6 +4,7 @@
     2. how to print all vector content and those of custmized types
 */
 #include <iostream>
+// #include <typeinfo>
 #include <vector>
 
 using std::cout;
@@ -21,13 +22,21 @@ public:
 
     // this function can be public or private, doesn't matter
     // it allows cout to reference private member length_ outside of the class
+    // TODO: I don't understand why friend is needed here but not when overloading ==, but if removed `friend` keyword we'll get many errors
     friend std::ostream& operator<<(std::ostream& stream, const Cube& other)
     {
         stream << other.length_;
         return stream;
     }
+
+    // overloading == to compare Cubes by length
+    bool operator==(const Cube& other)
+    {
+        return (length_ == other.length_);
+    }
 };
 
+// overloading cout to output cube_stack as std::vector type
 std::ostream& operator<<(std::ostream& stream, const std::vector<Cube>& other)
 {
     for (int i = 0; i < other.size(); i++) {
@@ -43,9 +52,14 @@ int main()
     Cube c1(1);
     Cube c2(2);
     Cube c3(3);
+    cout << "cube_stack vector capacity: " << cube_stack.capacity() << endl;
     cube_stack.push_back(c1);
+    cout << "cube_stack vector capacity: " << cube_stack.capacity() << endl;
     cube_stack.push_back(c2);
+    cout << "cube_stack vector capacity: " << cube_stack.capacity() << endl;
     cube_stack.push_back(c3);
+    cout << "cube_stack vector size: " << cube_stack.size() << endl;
+    cout << "cube_stack vector capacity: " << cube_stack.capacity() << endl; // notice that the capacity doubled from 2 to 4 here, larger than the size
     cout << cube_stack[0] << endl;
     cout << cube_stack[1] << endl;
     cout << cube_stack[2] << endl;
@@ -53,6 +67,13 @@ int main()
     cout << "last vector content: " << *(cube_stack.end() - 1) << endl; // alternatively (.end() returns iterator, not really a pointer)
     cout << "first vector content: " << cube_stack.front() << endl;
     cout << "first vector content: " << *cube_stack.begin() << endl; // alternatively
+
+    // below - finding which index c2(2) is at example code
+    for (int i = 0; i < cube_stack.size(); i++) {
+        if (c2 == cube_stack[i]) {
+            cout << "target is at index: " << i << endl;
+        }
+    }
 
     //below - printing cube stack exmaple
     cout << "cube stack is: " << cube_stack << endl;
