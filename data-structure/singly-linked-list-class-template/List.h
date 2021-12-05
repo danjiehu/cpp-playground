e8ddebb2d929866d3c12163d9669aa45953b687c
Danjie Hu <danjiehu@outlook.com>
2021-12-05 13:55:15 +0800

    completed singly linked list constructor and destructor

--- data-structure/singly-linked-list-class-template/List.h
+++ data-structure/singly-linked-list-class-template/List.h
@@ -18,20 +18,22 @@
 
 #include <iostream>
 
-// declaration
+// ! declaration
 template <typename T>
 class List {
 public:
     // public contains: class function declaration incl. [] operator, functions such as insertAtFront, constructor, destructor
-    void insertAtFront(const T& data);
-    // constructor - make sure head_is null-initialized
-    List()
-        : head_(nullptr) {};
-    // destructor - free the memory allocated for ListNode objects when the List is destroyed
-    ~List()
-    {
-        ListNode* thru = head_;
-    }
+    List();
+    ~List();
+
+    const T& operator[](unsigned index);
+    void insertAtFront(const T&);
+    void displayList();
+    void appendNode(const T&);
+    void deleteAtFront();
+    void deleteAtEnd();
+    void deleteNode(const T&);
+    void search(const T&);
 
 private:
     // private contains:  variables incl. ListNode class, ListNode head pointer (tail is nullptr), function such as search data
@@ -40,17 +42,42 @@ private:
     public:
         // if declared as private, List functions can't access it
         // ref: https://www.geeksforgeeks.org/nested-classes-in-c/
-        T m_data;
-        ListNode* m_next;
-        // ListNode constructor: m_data takes in data, m_next takes on nullptr
-        // passing by reference doesn't create copy, passing by const promise not to change the passed-in data in the constructor function
+        T node_data;
+        ListNode* next_ptr;
+
+        // ListNode default constructor: node_data takes in data, next_ptr takes on nullptr
         ListNode(const T& data)
-            : m_data(data)
-            , m_next(nullptr) {};
+            : node_data(data)
+            , next_ptr(nullptr) {};
     };
 
+    // ListNode pointer type: head pointer
     ListNode* head_;
 };
 
-// definition
+// ! definition
 // ! templated C++ code generally needs to be written entirely in header files, that's why we define the functions in header file, not cpp file here
+// or you can separate them into .h and .hpp file just for your own organization, for example append `#include "List.hpp"` at the end, ref: https://github.com/wadefagen/coursera/tree/master/linked-memory
+// 1. custom default constructor
+template <typename T>
+List<T>::List()
+    : head_(nullptr) {};
+
+// 2. custom default destructor - required for all class with heap allocation
+template <typename T> // TODO: use valgrind to check memory leak (effectiveness of the destructor)
+List<T>::~List()
+{
+    ListNode* curr = head_;
+    ListNode* toDelete;
+    while (curr != nullptr) {
+        toDelete = curr;
+        curr = curr->next; // obtained the next node pointer address before deleting current code
+        delete toDelete;
+        toDelete = nullptr;
+    }
+}
+
+template <typename T>
+List<T>::insertAtFront(const T& newNode)
+{
+}
