f17bb4d45cb74d962f170b104d32d76337072e82
Danjie Hu <danjiehu@outlook.com>
2021-12-07 15:56:12 +0800

    created memory folder

---
+++ cpp-syntax/memory/memory.cpp
@@ -0,0 +1,9 @@
+// demostrate
+// 1. the `new` keyword
+//  1.1 takes in an data type, so that it knows how much memory to ask for
+//  1.2 find the memory big enough for this much memory
+//  1.3 returns a pointer to the found memory address
+// 2. the `delete` keyword
+// 3. object lifetime
+// 4. dellocate memory automatically with smart pointers
+// 5. tracking memory without any third party program
