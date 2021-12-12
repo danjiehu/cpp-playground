d39a5fbca355c7f7fc8dd6f905e9df1d627672c2
Danjie Hu <danjiehu@outlook.com>
2021-12-12 17:19:45 +0800

    declared value binary tree class members

--- data-structure/value-binary-tree/ValueBinaryTree.h
+++ data-structure/value-binary-tree/ValueBinaryTree.h
@@ -4,29 +4,40 @@
 // this version has binary tree is value based
 // it stores value copies of items, rather than referecnes (`T data` rather than `T& data`)
 
+#include <iostream>
+#include <vector>
+
 template <typename T>
-class BinaryTree {
+class ValueBinaryTree {
 public:
     // todo: why declare treenode as public?
     class TreeNode {
     public:
-        T data;
+        T data; // this means data and dataArgument are only the same by value, they have different address; for more explanations, see examples in ref.cpp
         TreeNode* left;
         TreeNode* right;
 
         // initializing a treenode
-        // ! constructor makes a copy of "dataArgument" and initialize "data" with its value
-        // int data;
-        // const int& dataArgument = 8;
-        //
-        // T data = dataArgument
-        TreeNode(const T& dataArgument)
+        TreeNode(const T& dataArgument) // const T& dataArgument, this means dataArgument and a potential T type variable have the same address, this variable may or may not exist because you could still just construct a tree node like this `TreeNode(20)` without any additional variable, but just in case and we do need to pass in a variable in the future, we don't have to worry about the dataArgument copying its value cuz its a reference type
             : data(dataArgument)
             , left(nullptr)
             , right(nullptr) {};
-    }
+    };
 
-    TreeNode* root_;
+    ValueBinaryTree(); // default constructor
+    ValueBinaryTree(const std::vector<T>& contents); // custom constructor
+    ValueBinaryTree(const ValueBinaryTree& other); // copy constructor
+    ~ValueBinaryTree(); // desctructor;
+
+    void createCompelteTree(const std::vector<T>& contents);
+    void destroySubtree(TreeNode* subtreeRootPtr); // this function deletes the subtree rooted at the specified node; this is a recursive function.
+    void destroyWholeTree();
+
+    // traversal functions:
+    void preOrder(TreeNode* cur);
+    void inOrder(TreeNode* cur);
+    void postOrder(TreeNode* cur);
 
 private:
+    TreeNode* root_;
 };
