71bdf8161d5d3e635ac7bdf255fa6e098d1665fc
Danjie Hu <danjiehu@outlook.com>
2021-12-12 13:42:48 +0800

    update reference demo code remark on reference declaration

--- cpp-syntax/reference/ref.cpp
+++ cpp-syntax/reference/ref.cpp
@@ -12,6 +12,9 @@ int main()
     ptr = &var; // assign 0xB to be the value of ptr
     ptr = &foo; // assign 0xC to be the value of ptr
     int& ref = var; // created stack variable, address is 0xB, name is ref (also is var), value is 7 (it gave 0xB a different name)
+    // ! since a reference is an alias, the variable it's aliasing needs to be exist at initialization
+    // ! if int& ref - error: declaration of reference variable 'ref' requires an initializer
+    // ! however you can do this in class member variable, see `binary search tree` implementation code
     // notice how 0xA is not really used anywhere
 
     // accessing pointer's value
