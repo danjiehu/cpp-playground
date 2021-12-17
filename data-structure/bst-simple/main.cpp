82f6ea7b056ffeba5931150050c7a496f621c936
Danjie Hu <danjiehu@outlook.com>
2021-12-17 16:12:20 +0800

    added main executable file

--- data-structure/bst-simple/main.cpp
+++ data-structure/bst-simple/main.cpp
@@ -1,5 +1,7 @@
 // this implementation of the tree is simply written with Node class, without tree class
 // functions incl. 1. insert nodes 2. print tree 3. count nodes 4. computeHeight of node
+// memory leak check: leaks --atExit -- ./main
+
 #include <iostream>
 
 struct Node {
@@ -23,9 +25,15 @@ struct Node {
 
 // TODO
 
-main()
+int main()
 {
     Node* n = new Node();
+
+    n->left = new Node();
+    n->right = new Node();
+    n->right->left = new Node();
+    n->right->right = new Node();
+    n->right->right->right = new Node();
 
     // MUST delete head node
     // ! The Node destructor will recursively delete its children nodes.
