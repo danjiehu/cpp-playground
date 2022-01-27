acc4288a5aa6f57b40d0f79717827751b07a8fe6
Danjie Hu <danjiehu@outlook.com>
2022-01-27 10:28:37 +0800

    added error declare pointer without initialization

--- cpp-syntax/pitfalls/main.cpp
+++ cpp-syntax/pitfalls/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+// pitfall-1: declare pointer without initialization
+// int * iPtr; // this is pointing to an invalid address
+// *iPtr = 55; // can't find the invalid address, will throw error
+// cout << *iPtr << endl;
+// YOU CAN'T DECLARE A POINTER WITHOUT INITIALIZATION!
+
 int main()
 {
+
     return 0;
 }
