97e3a3212fa005c342297dad85b91430917788ca
Danjie Hu <danjiehu@192.168.0.101>
2022-04-06 18:09:13 +0800

    completed binary search iterative code demo

---
+++ search-algorithms/binary-search/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int binarySearch(int arr[], int value, int low, int high) // unsized array
+{
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (value == arr[mid]) {
+            return mid;
+        } else if (value > arr[mid]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[6] = { 2, 3, 4, 5, 6, 7 };
+
+    std::cout << binarySearch(arr, 10, 0, 5) << std::endl; // return -1;
+    std::cout << binarySearch(arr, 2, 0, 5) << std::endl; // return 0;
+    std::cout << binarySearch(arr, 6, 0, 5) << std::endl; // return 4;
+
+    return 0;
+}
