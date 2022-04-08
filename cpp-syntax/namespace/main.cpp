f6ccc67166f20c51e807a2bcf9e7728ab4e0b7a9
Danjie Hu <danjiehu@192.168.0.101>
2022-04-08 19:13:53 +0800

    completed namespace demo

--- cpp-syntax/namespace/main.cpp
+++ cpp-syntax/namespace/main.cpp
@@ -1,6 +1,39 @@
+// ref: https://www.coursera.org/learn/cs-fundamentals-1/lecture/hmEtj/1-3-c-s-standard-library-std
 #include <iostream>
+
+// namespace example
+// std::cout the std is the namespace to indicate that we are using the std standard library
+
+// usually is in header file
+namespace dhu {
+class Cube {
+public:
+    void setLength(int leng);
+    void printLength();
+
+private:
+    int length;
+};
+}
+
+// usually is in cpp file
+namespace dhu {
+void Cube::setLength(int leng)
+{
+    length = leng;
+};
+
+void Cube::printLength()
+{
+    std::cout << length << std::endl;
+}
+}
 
 int main()
 {
+    dhu::Cube c; // the dhu is the namespace
+    c.setLength(2);
+    c.printLength();
+
     return 0;
 }
