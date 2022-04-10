a403db7b362424d0e6d87d372377f5af92f74bdb
Danjie Hu <danjiehu@192.168.0.101>
2022-04-10 14:19:33 +0800

    added reasons for why create custom copy constructor and assignment operator in shallow-deep-copy demo

--- cpp-syntax/shallow-copy/shallow-copy.cpp
+++ cpp-syntax/shallow-copy/shallow-copy.cpp
@@ -1,22 +1,26 @@
 /*
-in shallow copy, variables in heap will NOT create a new copy.
+in shallow copy, we use default constructor and default assignment operator,
+- stack class member variable of the copiee object will use a new memory address
+- heap class member variable of the copiee object will use the SAME memory address of the copier object
+! that's why some times we need to write our own copy constructor and assignment operator
 */
 
 #include <iostream>
 using namespace std;
 
-class box
-{
+class box {
 private:
     int length;
     int breadth;
-    int *height;
+    int* height;
 
 public:
     void set_dimensions(int len, int brea, int heig)
     {
         length = len;
         breadth = brea;
+
+        height = new int;
         *height = heig;
     }
 
@@ -26,6 +30,12 @@ public:
              << "\n breadth is " << breadth << ", address is " << &breadth
              << "\n height is " << *height << ", address is " << height << endl;
     }
+
+    // ~box()
+    // {
+    //     delete height;
+    // }
+    // this program has memory leak
 };
 
 int main()
@@ -36,15 +46,17 @@ int main()
     b1.show_data();
     cout << "end of b1: original" << endl;
 
-    //automatic copy constructor;
+    // automatic copy constructor;
     box b2 = b1;
     b2.show_data();
+    // ! you'll see that address of b2.height is the same address of b1.height
     cout << "end of b2: copies b1" << endl;
 
-    //default assigment operator;
+    // automatic assigment operator;
     box b3;
     b3 = b1;
     b3.show_data();
+    // ! you'll see that address of b3.height is the same address of b1.height
     cout << "end of b3: assigned b1" << endl;
 
     return 0;
