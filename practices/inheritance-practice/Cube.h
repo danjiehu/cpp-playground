66271be88885fcab7042f63a83021a615a464ee8
Danjie Hu <danjiehu@DanjiedeMacBook.local>
2022-04-11 20:47:21 +0800

    completed inheritance practice

--- practices/inheritance-practice/Cube.h
+++ practices/inheritance-practice/Cube.h
@@ -1,4 +1,7 @@
 #include "Shape.h"
+#include <string>
+
+//  ! derived class only use all public members of the base class, NOT the private members
 
 // Cube is a inherited class from Shape
 // it has two new members
@@ -7,5 +10,32 @@
 //  1. public member function: getVolume()
 //  2. private member variable: color_
 
-class Cube : public Shape {
+class Cube : public Shape { // denote that Cube is a inherited class of Shape, always public
+public:
+    Cube(std::string color, double width);
+    double getVolume() const;
+
+private:
+    std::string color_;
 };
+
+Cube::Cube(std::string color, double width)
+    : Shape(width)
+    , color_(color) {};
+// this syntax is also ok
+// Cube::Cube(std::string color, double width)
+//     : Shape(width) // ! this is calling the base class constructor
+// {
+//     color_ = color; // this is unique to the Cube class
+// };
+
+double Cube::getVolume() const
+{
+    double width = getWidth();
+    return width * width * width;
+};
+
+// double Cube::getVolume() const
+// {
+//     return width_ * width_ * width_; // ! error: 'width_' is a private member of 'Shape'
+// };
--- practices/inheritance-practice/main.cpp
+++ practices/inheritance-practice/main.cpp
@@ -1,9 +1,13 @@
 // 1. how to create a inheritance class
 // 2. can a inherited class use parent class members?
 // 3. can parent class use inherited class members?
+#include "Cube.h"
 #include <iostream>
 
 int main()
 {
+    Cube c("purple", 2);
+    std::cout << c.getVolume() << std::endl;
+
     return 0;
 }
