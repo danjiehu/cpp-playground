a83ba9cf9fdee335a43329f8e04b0c17ac76fcb7
Danjie Hu <danjiehu@DanjiedeMacBook.local>
2022-04-11 17:29:22 +0800

    update folder

--- practices/template-practice/main.cpp
+++ practices/template-practice/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+// ex-1: template as function argument
+template <typename T>
+T max(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+// ex-2: template as class member variable
+
 int main()
 {
+    // ex-1:
+    std::cout << max(10, 20) << std::endl; // variable is defined when the function is called automatically
+    std::cout << max<double>(10.4, 20.12) << std::endl; // variable is defined when the function is called with an signed type
+
     return 0;
 }
