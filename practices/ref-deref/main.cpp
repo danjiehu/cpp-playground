2b024502e18f3cb8dbe3d3b090ad53ea3e49648f
Danjie Hu <danjiehu@DanjiedeMacBook.local>
2022-04-27 16:06:14 +0800

    added example 2 and 3 to demostrate the lifetime of function local variable, reference as function parameter, and pointer as function parameter

--- practices/ref-deref/main.cpp
+++ practices/ref-deref/main.cpp
@@ -2,10 +2,28 @@
 // reference has nothing to do with heap allocation, or the use of a pointer,
 // distinguish their differences below
 // dereference has to do with pointer
+void funcV(int* x) // ! x is a local variable that is discarded after the function ends, because this is not passing by reference this is passing by value
+{
+    int* y = new int;
+    *y = 16;
+    x = y; // x is same value as y now;
+    delete y; // destroy heap int 16;
+    // x goes out of scope, x is destroyped
+}
+
+void funcR(int*& x) // ! x is a local variable that is discarded after the function ends, because this is not passing by reference this is passing by value
+{
+    int* y = new int;
+    *y = 16;
+    x = y; // assigning y's value (heap address value) to x
+    std::cout << "funcR y value as of now: " << y << std::endl;
+    delete y; // destroy heap int 16;
+    // x is now the heap address value
+}
 
 int main()
 {
-    // pointer example
+    //* pointer example-1
     int* y;
     int x = 36;
     y = &x;
@@ -13,7 +31,23 @@ int main()
     std::cout << x << std::endl; // expect: 37 // y points to x's address
     std::cout << *y << std::endl; // expect: 37 // dereferenced pointer is the value at which the pointer points to
 
-    // heap allocation example
+    //* pointer type function parameter passed by value pointer type example-2
+    int i = 9;
+    funcV(&i); // this is passing i's address as parameter, it is NOT creating a reference
+    std::cout << i << std::endl; // expect: 9
+
+    //* pointer type function parameter passed by reference pointer type example-3
+    //! the key is at funciton definition
+    int j = 9;
+    int* j_address = &j; // ! j_address is an independent stack variable that temporarily stores the address of j
+    std::cout << "checking j_address before funcR: " << &j << std::endl; // expect: error or random value
+    funcR(j_address);
+    // ! note how you cannot use simple &j here for pointer reference parameter the other side of the reference type MUST be a variable that's already initialized
+    std::cout << "checking j_address after funcR: " << j_address << std::endl; // expect: same as funcR y value
+    std::cout << "checking j: " << j << std::endl; // expect: 6
+    std::cout << "checking &j: " << &j << std::endl; // expect: same as j_address before funcR
+
+    //* heap allocation example-4
     // no new no heap!!!
     int* heap_var = new int;
     *heap_var = 22;
@@ -27,11 +61,16 @@ int main()
     heap_var = nullptr;
     std::cout << heap_var << std::endl; // expect 0x0 for nullptr
     std::cout << &heap_var << std::endl; // expect the same stack address
-    std::cout << *heap_var << std::endl; // ! expect segmentation fault for dereferencing nullptr
+    // std::cout << *heap_var << std::endl; // ! expect segmentation fault for dereferencing nullptr
 
-    // reference example
+    //* reference example-5
     // ! reference has to reference an existing variable with value, the other side can NOT be address
-    // todo
+    int a = 30;
+    int& b = a;
+    std::cout << "a is int type, value is: " << a << std::endl;
+    std::cout << "b is int reference type, value is: " << b << std::endl;
+    std::cout << "a's address is: " << &a << std::endl;
+    std::cout << "b's address is: " << &b << std::endl; // the same as a
 
     return 0;
 }
